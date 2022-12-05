13e78bb5c2d23dc4f164dba0f2765a68ee15059d
AntaresMkII <abouemyannick@gmail.com>
2022-12-05 17:59:58 -0500

    Added first few tables in database

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -1,1 +1,36 @@
 -- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+
+CREATE TABLE genres (
+    name VARCHAR(20) PRIMARY KEY,
+    description VARCHAR(100)
+);
+
+CREATE TABLE authors (
+    id INT PRIMARY KEY,
+    bio VARCHAR(100),
+    nationality VARCHAR(20) NOT NULL,
+    fname VARCHAR(20) NOT NULL,
+    lname VARCHAR(20) NOT NULL,
+    dob DATE NOT NULL
+);
+
+CREATE TABLE publishers (
+    email VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(50),
+    bank_account VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE books (
+    isbn VARCHAR(10) PRIMARY KEY,
+    n_pages INT NOT NULL,
+    summary VARCHAR(100),
+    price NUMERIC(7,2),
+    title VARCHAR(50) NOT NULL,
+    release_date DATE,
+    in_stock INT NOT NULL,
+    publisher_percentage INT NOT NULL,
+    p_email VARCHAR(20) NOT NULL,
+    FOREIGN KEY (p_email)
+        REFERENCES publishers (email)
+);

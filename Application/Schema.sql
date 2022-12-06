d9a7525c7e72847fcfd5212f0cddddcc669bc115
AntaresMkII <abouemyannick@gmail.com>
2022-12-06 09:41:07 -0500

    added more schemas in database

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -34,3 +34,62 @@ CREATE TABLE books (
     FOREIGN KEY (p_email)
         REFERENCES publishers (email)
 );
+
+CREATE TABLE users (
+    email VARCHAR(50) PRIMARY KEY,
+    lname VARCHAR(20) NOT NULL,
+    fname VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE checkout_baskets (
+    email VARCHAR(50) PRIMARY KEY,
+    FOREIGN KEY (email)
+        REFERENCES users (email)
+);
+
+CREATE TABLE orders (
+    number INT PRIMARY KEY,
+    order_date DATE NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    FOREIGN KEY (email)
+        REFERENCES users (email)
+);
+
+CREATE TABLE billing_infos (
+    order_number INT PRIMARY KEY,
+    address VARCHAR(50) NOT NULL,
+    fname VARCHAR(20) NOT NULL,
+    lname VARCHAR(20) NOT NULL,
+    card_number CHAR(16) NOT NULL,
+    exp_date DATE NOT NULL,
+    cvv CHAR(3) NOT NULL,
+    FOREIGN KEY (order_number)
+        REFERENCES orders (number)
+);
+
+CREATE TABLE shipping_infos (
+    order_number INT PRIMARY KEY,
+    address VARCHAR(50) NOT NULL,
+    fname VARCHAR(20) NOT NULL,
+    lname VARCHAR(20) NOT NULL,
+    FOREIGN KEY (order_number)
+        REFERENCES orders (number)
+);
+
+CREATE TABLE tracking_infos (
+    order_number INT PRIMARY KEY,
+    curier VARCHAR(20) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    exp_delivery_date DATE NOT NULL,
+    FOREIGN KEY (order_number)
+        REFERENCES orders (number)
+);
+
+CREATE TABLE is_genre (
+    genre_name VARCHAR(20) PRIMARY KEY,
+    isbn VARCHAR(10) PRIMARY KEY,
+    FOREIGN KEY (genre_name)
+        REFERENCES genre (name),
+    FOREIGN KEY (isbn)
+        REFERENCES books (isbn)
+);
